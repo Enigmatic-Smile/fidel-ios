e1c6f4b4e8aa46997caeafb262b7cd3619a0b725
Cornel Chitanu <tap@dolfn.com>
2021-05-13 09:53:04 +0200

    Add Objective-C Cocoapods example application.

---
+++ Examples/ObjCCocoapodsExample/ObjCCocoapodsExample/FidelExampleViewController.h
@@ -0,0 +1,14 @@
+//
+//  FidelExampleViewController.h
+//  ObjCCocoapodsExample
+//
+//  Created by Corneliu on 13.05.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FidelExampleViewController : UIViewController
+
+
+@end
+
